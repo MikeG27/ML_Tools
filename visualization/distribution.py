1c9224477a8b8d5a4262b9bb8a4161c61a94421e
= <gorskimichal27@gmail.com>
2019-12-05 12:31:06 +0100

    update ecdf function

--- visualization/distribution.py
+++ visualization/distribution.py
@@ -6,13 +6,25 @@ from statsmodels.distributions.empirical_distribution import ECDF
 
 sns.set()
 
-def ecdf(df_feature,xlabel):
-    ecdf = ECDF(df_feature)
-    plt.title("ECDF Plot ")
-    plt.plot(ecdf.x, ecdf.y)
-    plt.xlabel(xlabel)
-    return ecdf
+# def ecdf(df_feature,xlabel):
+#     ecdf = ECDF(df_feature)
+#     plt.title("ECDF Plot ")
+#     plt.plot(ecdf.x, ecdf.y)
+#     plt.xlabel(xlabel)
+#     return ecdf
 
+
+def ecdf(data, title="ECDF Plot", xlabel='Data Values', ylabel='Percentage'):
+    """
+    Function to plot ecdf taking a column of data as input.
+    """
+    xaxis = np.sort(data)
+    yaxis = np.arange(1, len(data) + 1) / len(data)
+    plt.plot(xaxis, yaxis, linestyle='none', marker='.')
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.margins(0.02)
 
 def distribution(df_feature,xlabel):
     bins = int(np.sqrt(len(df_feature)))
