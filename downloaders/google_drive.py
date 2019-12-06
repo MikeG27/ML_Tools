20dd1bb080f0266c1ff768cc49681c4d188a2b0a
= <gorskimichal27@gmail.com>
2019-12-06 11:56:30 +0100

    init

---
+++ downloaders/google_drive.py
@@ -0,0 +1,5 @@
+from google_drive_downloader import GoogleDriveDownloader as gdd
+
+gdd.download_file_from_google_drive(file_id='1E6YhO0sCFUbNOs_4IpgS4uq_ZcuYb_e1',
+                                    dest_path='./data/Panel ChallengING.zip',
+                                    unzip=True)
