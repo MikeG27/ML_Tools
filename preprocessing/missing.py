4494b40ce73fa659b9ebb0374dffcbdc17d8ff00
= <gorskimichal27@gmail.com>
2019-12-10 14:56:11 +0100

    add basic imputer example

--- preprocessing/missing.py
+++ preprocessing/missing.py
@@ -1,25 +1,37 @@
+import numpy as np
 import pandas as pd
-from pandas import DataFrame
+from sklearn.impute import SimpleImputer
 
-
-def missing_values_counter(df, threshold=0.3) -> DataFrame:
+def missing_values_counter(df):
     total = df.isnull().sum().sort_values(ascending=False)
-    percent = (df.isnull().sum() / len(df) * 100).sort_values(ascending=False)
+    percent = (df.isnull().mean() * 100).sort_values(ascending=False)
     dtype = df.dtypes
 
     missing_data = pd.concat([total, percent, dtype], axis=1, keys=['Total', 'Percent', "Dtype"]).sort_values("Percent",
                                                                                                               ascending=False)
-    missing_data = missing_data[missing_data["Percent"] > threshold]
 
-    print("Your selected dataframe has " + str(df.shape[1]) + " columns and " + str(df.shape[0]) + " Rows.\n"
-                                                                                                   "There are " + str(
+    print("Your selected dataframe has " + str(df.shape[1]) + " columns and " + str(
+        df.shape[0]) + ' Rows.\n''There are ' + str(
         missing_data.shape[0]) + f" columns that have missing values above {threshold * 100}%")
 
     return missing_data
 
 
-def dropna_rows_below_column_threshold(df,df_miss,threshold=3):
-    missing_below_threshold_list = df_miss.iloc[1][df_miss.iloc[1] < threshold].index.tolist()
-    df.dropna(subset=[*missing_below_threshold_list])
+def missing_imputer(df, columns, strategy="most_frequent"):
+    """
+    :param df: Pandas dataframe
+    :param columns: target columns for imputer
+    :param strategy: Imputation strategy
 
-    return df
+    If “mean”, then replace missing values using the mean along each column. Can only be used with numeric data.
+    If “median”, then replace missing values using the median along each column. Can only be used with numeric data.
+    If “most_frequent”, then replace missing using the most frequent value along each column. Can be used with strings or numeric data.
+    If “constant”, then replace missing values with fill_value. Can be used with strings or numeric data.
+
+    :return: imputer instance
+    """
+    imputer = SimpleImputer(missing_values=np.nan, strategy=strategy)
+    df[columns] = imputer.fit_transform(df[columns])
+    return imputer
+
+
